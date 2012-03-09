cb708e8c2debc3853ab389e69cfec0a879a12ca1
Jan Odvarko <odvarko@gmail.com>
2012-03-09 13:37:15 +0100

    Support for dynamic append/remove of a side panel.

--- SidePanel/chrome/content/myPanel.js
+++ SidePanel/chrome/content/myPanel.js
@@ -3,12 +3,17 @@
 define([
     "firebug/lib/lib",
     "firebug/lib/trace",
+    "sidepanel/mySidePanel",
 ],
-function(FBL, FBTrace) {
+function(FBL, FBTrace, MySidePanel) {
 
 // ********************************************************************************************* //
 // Panel Implementation
 
+/**
+ * This object represents a main Firebug panel. There are also toolbar buttons that allows
+ * to dynamically append and remove a side panel.
+ */
 var MyPanel = function MyPanel() {};
 MyPanel.prototype = FBL.extend(Firebug.Panel,
 {
@@ -27,6 +32,62 @@ MyPanel.prototype = FBL.extend(Firebug.Panel,
     {
         Firebug.Panel.destroy.apply(this, arguments);
     },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    /**
+     * Extends toolbar for this panel.
+     */
+    getPanelToolbarButtons: function()
+    {
+        var buttons = [];
+
+        // Check whether our side panel is registered.
+        var registered = Firebug.getPanelType("mySidePanel");
+
+        buttons.push({
+            id: "myRemoveSidePanelButton",
+            label: "Remove Side Panel",
+            disabled: !registered,
+            command: FBL.bindFixed(this.onRemoveSidePanel, this)
+        });
+
+        buttons.push({
+            id: "myAppendSidePanelButton",
+            label: "Append Side Panel",
+            disabled: registered,
+            command: FBL.bindFixed(this.onAppendSidePanel, this)
+        });
+
+        return buttons;
+    },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    updateToolbar: function()
+    {
+        var removeBtn = Firebug.chrome.$("myRemoveSidePanelButton");
+        var appendBtn = Firebug.chrome.$("myAppendSidePanelButton");
+
+        var registered = Firebug.getPanelType("mySidePanel");
+        removeBtn.setAttribute("disabled", registered ? "false" : "true");
+        appendBtn.setAttribute("disabled", registered ? "true" : "false");
+    },
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Commands
+
+    onRemoveSidePanel: function()
+    {
+        Firebug.unregisterPanel(MySidePanel);
+        this.updateToolbar();
+    },
+
+    onAppendSidePanel: function()
+    {
+        Firebug.registerPanel(MySidePanel);
+        this.updateToolbar();
+    }
 });
 
 // ********************************************************************************************* //
