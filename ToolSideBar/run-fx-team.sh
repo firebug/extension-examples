0c5aad34df3f7841e6e7bf851feae40cf17bbd02
Jan Odvarko <odvarko@gmail.com>
2014-08-20 14:57:25 +0200

    Remove obsolete files

--- ToolSideBar/run-fx-team.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cfx run \
-    -o \
-    -b C:/src/mozilla.org/fx-team/obj-i686-pc-mingw32/dist/bin/firefox.exe \
-
--- ToolSideBar/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cfx run \
-    -o \
-    --no-strip-xpi \
-    -b C:/src/mozilla.org/fx-team/obj-i686-pc-mingw32/dist/bin/firefox.exe \
-    --profiledir C:/Users/Honza/AppData/Roaming/Mozilla/Firefox/Profiles/ug2k4vci.nightlyNext002
-
