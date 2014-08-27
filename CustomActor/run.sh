4ac879451f570fcd23d1548281d8635ab409811b
Jan Odvarko <odvarko@gmail.com>
2014-08-27 09:54:26 +0200

    Comment in the run.sh script

--- CustomActor/run.sh
+++ CustomActor/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Use your own paths to Firefox binary (-b) and your Firefox
+# profile for development (--profiledir)
+# Read more: https://developer.mozilla.org/en-US/Add-ons/SDK/Tools/cfx
 cfx run \
-    -o \
-    --no-strip-xpi \
-    -b C:/mozilla-dev/firefox/firefox-nightly/firefox.exe \
-    --profiledir C:/Users/Honza/AppData/Roaming/Mozilla/Firefox/Profiles/ug2k4vci.nightlyNext002
+  -o \
+  --no-strip-xpi \
+  -b C:/mozilla-dev/firefox/firefox-nightly/firefox.exe \
+  --profiledir C:/Users/Honza/AppData/Roaming/Mozilla/Firefox/Profiles/ug2k4vci.nightlyNext002
